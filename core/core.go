a4585e5c03c66dc85830569c8827389ee8e55da6
tesh254 <ewachira254@gmail.com>
2025-08-09 20:34:20 +0300

    add more core public APIs

--- core/core.go
+++ core/core.go
@@ -5,6 +5,21 @@ func ReadFile(path string) ([]byte, error) {
 	return readFile(path)
 }
 
+// GetFileLineMap reads and parses a file to line map.
+// The line number being the key and the contents being the value.
+func FileToLineMap(path string) (map[string]string, error) {
+	content, err := readFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return toLineMap(string(content)), nil
+}
+
+// LineMapToFile converts a line map back to a file content.
+func LineMapToFile(lineMap map[string]string) string {
+	return fromLineMap(lineMap)
+}
+
 // WriteFile writes data to a file at the given path.
 func WriteFile(path string, data []byte) error {
 	return writeFile(path, data)
